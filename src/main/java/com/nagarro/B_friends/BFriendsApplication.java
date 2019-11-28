bd9b24ef88605e5f7f53db99da9a403d4b1bbeb4
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-11-28 11:54:42 +0530

    Sample AOP integration

--- src/main/java/com/nagarro/B_friends/BFriendsApplication.java
+++ src/main/java/com/nagarro/B_friends/BFriendsApplication.java
@@ -2,6 +2,7 @@ package com.nagarro.B_friends;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * 
@@ -16,6 +17,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class BFriendsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/nagarro/B_friends/aspect/FriendServiceAspect.java
@@ -0,0 +1,25 @@
+package com.nagarro.B_friends.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FriendServiceAspect {
+	@Before(value = "execution(* com.nagarro.B_friends.service.FriendService.*(..)) and args(firstName,lastName)")
+	public void beforeAdvice(JoinPoint joinPoint, String firstName, String lastName) {
+		System.out.println("Before method:" + joinPoint.getSignature());
+
+		System.out.println("Creating Friend with name - " + firstName + " and id - " + lastName);
+	}
+
+	@After(value = "execution(* com.nagarro.B_friends.service.FriendService.*(..)) and args(firstName,lastName)")
+	public void afterAdvice(JoinPoint joinPoint, String firstName, String lastName) {
+		System.out.println("After method:" + joinPoint.getSignature());
+
+		System.out.println("Successfully created Friend with name - " + firstName + " and id - " + lastName);
+	}
+}
